502b56080d1374dbc5fa594afdbc2f020103d75f
Emile M <emile.moncel@gmail.com>
2026-01-12 15:23:40 +0100

    Ajout de la vérification d'autorisation de parsing et du parsing html à l'aide de Beautiful Soup. Peut être mettre le USER_AGENT dans un .env à l'avenir

---
+++ TP1/Parsing.py
@@ -0,0 +1,22 @@
+from urllib.robotparser import RobotFileParser
+from urllib.parse import urlparse
+
+#Crawler
+USER_AGENT = "Crawler/1.0 (+emile.moncel@eleve-ensai.fr)"
+
+def parsing_allowed(url):
+    parsed = urlparse(url)
+    robots_url = f"{parsed.scheme}://{parsed.netloc}/robots.txt"  #Pour avoir un lien de la forme nom_de_domaine/robots.txt
+
+    rp = RobotFileParser()
+    rp.set_url(robots_url) #Modifie l'URL référençant le fichier robots.txt.
+    rp.read()
+
+    return rp.can_fetch(USER_AGENT, url) #Renvoie True si useragent est autorisé à accéder à url selon les règles contenues dans le fichier robots.txt analysé.
+
+
+from bs4 import BeautifulSoup
+#Parse une page html
+def parsing_html(html_doc):
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    return(soup)
